cfa4b6ff2d542d213e909ba3fcf0311bb898a5ce
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-08-09 22:46:21 +0530

    09

--- MySchool.Entities/GradeEntities.cs
+++ MySchool.Entities/GradeEntities.cs
@@ -11,7 +11,6 @@ namespace MySchool.Entities
         public int ID { get; set; }
         public string Grade { get; set; }
         public int GradeGroupID { get; set; }
-        //public string GradeGroup { get; set; }
-        ////gdhd
+        public string GradeGroup { get; set; }
     }
 }
---
+++ MySchool.UI/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySchool.BL;
+using MySchool.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace MySchool.UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

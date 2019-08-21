d48090bc9f9c699a6da44cb880f8b721bd0bed09
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-08-21 17:51:32 +0530

    PP_Patch_21_08_2019

---
+++ MySchool.BL/StudentParticularsBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySchool.Entities;
+using MySchool.DAL;
+
+namespace MySchool.BL
+{
+    public class StudentParticularsBL
+    {
+        //for StudentName drp
+        public List<StudentParticularsEntities> GetStudentForDrp()
+        {
+            StudentParticularsDAL studentParticularsDal = new StudentParticularsDAL();
+            return studentParticularsDal.GetStudentForDrp();
+        }
+
+        //for Grade drp
+        public List<StudentParticularsEntities> GetGradeForDrp()
+        {
+            StudentParticularsDAL studentParticularsDal = new StudentParticularsDAL();
+            return studentParticularsDal.GetGradeForDrp();
+        }
+
+        //for Section drp
+        public List<StudentParticularsEntities> GetSectionForDrp()
+        {
+            StudentParticularsDAL studentParticularsDal = new StudentParticularsDAL();
+            return studentParticularsDal.GetSectionForDrp();
+        }
+    }
+}
--- MySchool.UI/Controllers/StudentHealthController.cs
+++ MySchool.UI/Controllers/StudentHealthController.cs
@@ -16,7 +16,94 @@ namespace MySchool.UI.Controllers
         // GET: StudentHealth
         public ActionResult Index()
         {
+            GetSectionForDrp();
+            GetStudentForDrp();
+            GetGradeForDrp();
             return View();
+        }
+
+        //for Catagory drp
+        public void GetStudentForDrp()
+        {
+            StudentParticularsBL studentParticularsBL = new StudentParticularsBL();
+            List<StudentParticularsEntities> pocoList = new List<StudentParticularsEntities>();
+
+            pocoList = studentParticularsBL.GetStudentForDrp();
+
+            List<StudentParticularsEntities> itmasterList = new List<StudentParticularsEntities>();
+
+            foreach (StudentParticularsEntities up in pocoList)
+            {
+                StudentParticularsEntities unt = new StudentParticularsEntities();
+                unt.StudentID = up.StudentID;
+                unt.StudentName = up.StudentName;
+
+                itmasterList.Add(unt);
+            }
+
+            ViewBag.StudentParticulars = itmasterList.Select(x =>
+                                            new SelectListItem()
+                                            {
+                                                Text = x.StudentName,
+                                                Value = x.StudentID.ToString()
+                                            });
+
+        }
+
+        //for Grade drp
+        public void GetGradeForDrp()
+        {
+            StudentParticularsBL studentParticularsBL = new StudentParticularsBL();
+            List<StudentParticularsEntities> pocoList = new List<StudentParticularsEntities>();
+
+            pocoList = studentParticularsBL.GetGradeForDrp();
+
+            List<StudentParticularsEntities> itmasterList = new List<StudentParticularsEntities>();
+
+            foreach (StudentParticularsEntities up in pocoList)
+            {
+                StudentParticularsEntities unt = new StudentParticularsEntities();
+                unt.GradeID = up.GradeID;
+                unt.Grade = up.Grade;
+
+                itmasterList.Add(unt);
+            }
+
+            ViewBag.GradeForDrp = itmasterList.Select(x =>
+                                            new SelectListItem()
+                                            {
+                                                Text = x.Grade,
+                                                Value = x.GradeID.ToString()
+                                            });
+
+        }
+
+        //for Section drp
+        public void GetSectionForDrp()
+        {
+            StudentParticularsBL studentParticularsBL = new StudentParticularsBL();
+            List<StudentParticularsEntities> pocoList = new List<StudentParticularsEntities>();
+
+            pocoList = studentParticularsBL.GetSectionForDrp();
+
+            List<StudentParticularsEntities> itmasterList = new List<StudentParticularsEntities>();
+
+            foreach (StudentParticularsEntities up in pocoList)
+            {
+                StudentParticularsEntities unt = new StudentParticularsEntities();
+                unt.SectionID = up.SectionID;
+                unt.Section = up.Section;
+
+                itmasterList.Add(unt);
+            }
+
+            ViewBag.SectionForDrp = itmasterList.Select(x =>
+                                            new SelectListItem()
+                                            {
+                                                Text = x.Section,
+                                                Value = x.SectionID.ToString()
+                                            });
+
         }
     }
 }

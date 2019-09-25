2164124beb547a92786fec8b75ab85a034419be0
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-09-25 12:07:51 +0530

    25.II

---
+++ MySchool.UI/Controllers/ResultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySchool.BL;
+using MySchool.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace MySchool.UI.Controllers
+{
+    public class ResultController : Controller
+    {
+        // GET: Result
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MockTestResult()
+        {
+            return View();
+        }
+
+        public ActionResult RealExamResult()
+        {
+            return View();
+        }
+    }
+}

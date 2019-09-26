fa1590c0c8777536921e97ec284e29ba90986811
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-09-26 11:57:54 +0530

    26.I

--- MySchool.UI/Scripts/Question.js
+++ MySchool.UI/Scripts/Question.js
@@ -27,6 +27,8 @@ function clearTextBox() {
     $('#Marks').val("");
     $('#drpSubject').val("");
     $('#tblSubject').val("");
+
+   // $('input:checkbox').removeAttr('checked');       // chkPrimarySubject
 }
 
 function Add() {

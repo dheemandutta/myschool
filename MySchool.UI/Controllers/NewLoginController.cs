cc387e86e3dd0bb716a41a57e4f34b60265f6670
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-10-30 13:38:22 +0530

    ActualAdmission

---
+++ MySchool.UI/Controllers/NewLoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySchool.BL;
+using MySchool.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Web.Security;
+
+namespace MySchool.UI.Controllers
+{
+    public class NewLoginController : Controller
+    {
+        // GET: NewLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
